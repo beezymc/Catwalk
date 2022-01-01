392c96192cc32ae9140267d0ccc5c2e0b06577c5
David Rajec <david.rajec@gmail.com>
2021-12-31 17:29:35 -0800

    Completed jest config debugging

--- client/src/components/app.test.jsx
+++ client/src/components/app.test.jsx
@@ -1,20 +1,24 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react'
-// import App from './index.jsx';
-// import '@testing-library/jest-dom'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AppContainer from '../redux/containers/AppContainer.js';
+import '@testing-library/jest-dom';
+import store from '../redux/store/store.js';
+import { Provider } from 'react-redux';
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  });
+});
 
 
 // describe('App', () => {
 //   test('renders App component', () => {
-//     render(<App />)
-//   });
-// });
-
-
-// describe('App', () => {
-//   test('renders App component', () => {
-//     render(<App />);
-
+//     render(<AppContainer />);
 //     expect(screen.getByText('Project Catwalk')).toBeInTheDocument();
 //   });
 // });
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,33 @@
-module.export = {
-  roots: ['<rootDir>/src'],
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node', 'css'],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+  roots: ['<rootDir>/client/src'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
   transform: {
     '\\.(js|jsx)?$': 'babel-jest',
   },
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/>(*.)test.{js, jsx}'], // finds test
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/public/'],
-  setupFilesAfterEnv: [
-    '@testing-library/jest-dom/extend-expect',
-    '@testing-library/react/cleanup-after-each'
-  ] // setupFiles before the tests are ran
 };
