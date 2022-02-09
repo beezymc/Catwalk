7b98b2d800b13bcf6d12d1d13cc2afe1d281b5c9
David Rajec <david.rajec@gmail.com>
2022-02-09 15:12:20 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+/* Include a github token, the path to the API, and the Port number below */
+const token = '';
+const API_path = '';
+const Port = ;
+
+module.exports = {
+  token,
+  API_path,
+  Port
+};
